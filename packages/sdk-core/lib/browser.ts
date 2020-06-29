f16d38aeaea4a6bd130d0f1df8666ddf58f62f75
Justin Fiedler <justin@iterative.ly>
2020-06-29 08:48:47 -0700

    chore: re-added isomorphic changes

---
+++ packages/sdk-core/lib/browser.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars, class-methods-use-this, import/extensions, import/no-unresolved */
+import itlySdk, {
+  Options,
+  Event, Properties,
+} from './base';
+
+// Itly Browser SDK
+class Itly {
+  load = (
+    options: Options,
+  ) => itlySdk.load(options);
+
+  alias = (
+    userId: string, previousId?: string,
+  ) => itlySdk.alias(userId, previousId);
+
+  identify = (
+    userId: string | undefined, identifyProperties?: Properties,
+  ) => itlySdk.identify(userId, identifyProperties);
+
+  group = (
+    groupId: string, groupProperties?: Properties,
+  ) => itlySdk.group(undefined, groupId, groupProperties);
+
+  page = (
+    category: string, name: string, pageProperties?: Properties,
+  ) => itlySdk.page(undefined, category, name, pageProperties);
+
+  track = (
+    event: Event,
+  ) => itlySdk.track(undefined, event);
+
+  reset = () => itlySdk.reset();
+
+  getPlugin = (id: string) => itlySdk.getPlugin(id);
+}
+
+export default new Itly();
---
+++ packages/sdk-core/lib/node.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars, class-methods-use-this, import/extensions, import/no-unresolved */
+import itly from './base';
+
+export default itly;
