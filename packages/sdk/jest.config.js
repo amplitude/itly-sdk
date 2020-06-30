03e166f63c4ab155e915ee4f1a1da30940c3eb96
Justin Fiedler <justin@iterative.ly>
2020-06-30 15:50:55 -0700

    chore: removed sdk and sdk-node

--- packages/sdk/jest.config.js
+++
@@ -1,9 +0,0 @@
-// Jest configuration for api
-const base = require('../../jest.config.base.js');
-const packageJson = require('./package.json');
-
-module.exports = {
-  ...base,
-  name: packageJson.name,
-  displayName: packageJson.name,
-};
