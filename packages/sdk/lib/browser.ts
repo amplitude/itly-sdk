72b7915742fbd0519b471b18c4f00d6d23bccd69
Andrey Sokolov <falconandy@yandex.ru>
2020-12-16 20:19:16 +0400

    fix: added code docs to all SDK methods

--- packages/sdk/lib/browser.ts
+++ packages/sdk/lib/browser.ts
@@ -28,14 +28,29 @@ export {
 
 // Itly Browser SDK
 export class ItlyBrowser {
+  /**
+   * Initialize the Itly SDK. Call once when your application starts.
+   * @param context Additional context properties to add to all events.
+   * @param options Configuration options to initialize the Itly SDK with.
+   */
   load = (
     context?: Properties, options?: Options,
   ) => itlyBase.load(context, options);
 
+  /**
+   * Alias a user ID to another user ID.
+   * @param userId The user's new ID.
+   * @param previousId The user's previous ID.
+   */
   alias = (
     userId: string, previousId?: string,
   ) => itlyBase.alias(userId, previousId);
 
+  /**
+   * Identify a user and set or update that user's properties.
+   * @param userId The user's ID.
+   * @param identifyProperties The user's properties.
+   */
   identify = (
     userId: string | Properties | undefined, identifyProperties?: Properties,
   ) => {
@@ -49,18 +64,41 @@ export class ItlyBrowser {
     itlyBase.identify(userId, identifyProperties);
   }
 
+  /**
+   * Associate the current user with a group and set or update that group's properties.
+   * @param groupId The group's ID.
+   * @param groupProperties The group's properties.
+   */
   group = (
     groupId: string, groupProperties?: Properties,
   ) => itlyBase.group(undefined, groupId, groupProperties);
 
+  /**
+   * Track a page view.
+   * @param category The page's category.
+   * @param name The page's name.
+   * @param pageProperties The page's properties.
+   */
   page = (
     category: string, name: string, pageProperties?: Properties,
   ) => itlyBase.page(undefined, category, name, pageProperties);
 
+  /**
+   * Track any event.
+   * @param event The event.
+   * @param event.name The event's name.
+   * @param event.properties The event's properties.
+   * @param event.id The event's ID.
+   * @param event.version The event's version.
+   * @param event.metadata The event's metadata.
+   */
   track = (
     event: Event,
   ) => itlyBase.track(undefined, event);
 
+  /**
+   * Reset (e.g. on logout) all analytics state for the current user and group.
+   */
   reset = () => itlyBase.reset();
 }
 
