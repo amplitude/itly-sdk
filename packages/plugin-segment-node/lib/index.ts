adb136a650c1be36410e1d2daea594f38d3462f7
Andrey Sokolov <falconandy@yandex.ru>
2021-01-15 21:09:13 +0400

    feat: log Segment (Node) requests

--- packages/plugin-segment-node/lib/index.ts
+++ packages/plugin-segment-node/lib/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars, class-methods-use-this */
 import {
-  Plugin, Event, Properties,
+  Plugin, Event, Properties, Logger, PluginLoadOptions,
 } from '@itly/sdk';
 import Segment from 'analytics-node';
 
@@ -17,6 +17,8 @@ export type SegmentOptions = {
 export class SegmentPlugin extends Plugin {
   private segment?: Segment;
 
+  private logger: Logger | undefined;
+
   constructor(
     private writeKey: string,
     private options?: SegmentOptions,
@@ -24,43 +26,92 @@ export class SegmentPlugin extends Plugin {
     super('segment');
   }
 
-  load() {
+  load(options: PluginLoadOptions) {
     this.segment = new Segment(this.writeKey, this.options);
+    this.logger = options.logger;
   }
 
   alias(userId: string, previousId: string) {
-    this.segment!.alias({ userId, previousId });
+    const id = +new Date();
+    const payload = {
+      userId,
+      previousId,
+    };
+    this.logger!.debug(`${this.id}: alias(request) ${id}: ${JSON.stringify(payload)}`);
+    this.segment!.alias(payload, (err: Error | undefined) => {
+      if (err == null) {
+        this.logger!.debug(`${this.id}: alias(response) ${id}: success`);
+      } else {
+        this.logger!.error(`${this.id}: alias(response) ${id}: ${err}`);
+      }
+    });
   }
 
   identify(userId: string, properties: Properties | undefined) {
-    this.segment!.identify({
+    const id = +new Date();
+    const payload = {
       userId,
       traits: { ...properties },
+    };
+    this.logger!.debug(`${this.id}: identify(request) ${id}: ${JSON.stringify(payload)}`);
+    this.segment!.identify(payload, (err: Error | undefined) => {
+      if (err == null) {
+        this.logger!.debug(`${this.id}: identify(response) ${id}: success`);
+      } else {
+        this.logger!.error(`${this.id}: identify(response) ${id}: ${err}`);
+      }
     });
   }
 
   group(userId: string, groupId: string, properties: Properties | undefined) {
-    this.segment!.group({
+    const id = +new Date();
+    const payload = {
       userId,
       groupId,
       traits: properties,
+    };
+    this.logger!.debug(`${this.id}: group(request) ${id}: ${JSON.stringify(payload)}`);
+    this.segment!.group(payload, (err: Error | undefined) => {
+      if (err == null) {
+        this.logger!.debug(`${this.id}: group(response) ${id}: success`);
+      } else {
+        this.logger!.error(`${this.id}: group(response) ${id}: ${err}`);
+      }
     });
   }
 
   page(userId: string, category: string, name: string, properties: Properties | undefined) {
-    this.segment!.page({
+    const id = +new Date();
+    const payload = {
       userId,
       category,
       name,
       properties,
+    };
+    this.logger!.debug(`${this.id}: page(request) ${id}: ${JSON.stringify(payload)}`);
+    this.segment!.page(payload, (err: Error | undefined) => {
+      if (err == null) {
+        this.logger!.debug(`${this.id}: page(response) ${id}: success`);
+      } else {
+        this.logger!.error(`${this.id}: page(response) ${id}: ${err}`);
+      }
     });
   }
 
   track(userId: string, event: Event) {
-    this.segment!.track({
+    const id = +new Date();
+    const payload = {
       userId,
       event: event.name,
       properties: { ...event.properties },
+    };
+    this.logger!.debug(`${this.id}: track(request) ${id}: ${JSON.stringify(payload)}`);
+    this.segment!.track(payload, (err: Error | undefined) => {
+      if (err == null) {
+        this.logger!.debug(`${this.id}: track(response) ${id}: success`);
+      } else {
+        this.logger!.error(`${this.id}: track(response) ${id}: ${err}`);
+      }
     });
   }
 }
