cd0afa7b02702ee2567717ad736ed56443359a24
Andrey Sokolov <falconandy@yandex.ru>
2021-01-18 20:25:50 +0400

    feat: log Mixpanel requests

--- packages/plugin-mixpanel/lib/index.ts
+++ packages/plugin-mixpanel/lib/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars, class-methods-use-this, import/no-unresolved */
 import {
-  Options, Event, Properties, Plugin,
+  Event, Properties, Plugin, Logger, PluginLoadOptions,
 } from '@itly/sdk';
 
 export type MixpanelOptions = {};
@@ -15,6 +15,8 @@ export class MixpanelPlugin extends Plugin {
     return s && s.mixpanel;
   }
 
+  private logger: Logger | undefined;
+
   constructor(
     private apiKey: string,
     private options?: MixpanelOptions,
@@ -22,7 +24,7 @@ export class MixpanelPlugin extends Plugin {
     super('mixpanel');
   }
 
-  load() {
+  load(options: PluginLoadOptions) {
     if (!this.mixpanel) {
       // Mixpanel (https://developer.mixpanel.com/docs/javascript)
       // @ts-ignore
@@ -30,6 +32,7 @@ export class MixpanelPlugin extends Plugin {
       (function(c,a){if(!a.__SV){var b=window;try{var d,m,j,k=b.location,f=k.hash;d=function(a,b){return(m=a.match(RegExp(b+"=([^&]*)")))?m[1]:null};f&&d(f,"state")&&(j=JSON.parse(decodeURIComponent(d(f,"state"))),"mpeditor"===j.action&&(b.sessionStorage.setItem("_mpcehash",f),history.replaceState(j.desiredHash||"",c.title,k.pathname+k.search)))}catch(n){}var l,h;window.mixpanel=a;a._i=[];a.init=function(b,d,g){function c(b,i){var a=i.split(".");2==a.length&&(b=b[a[0]],i=a[1]);b[i]=function(){b.push([i].concat(Array.prototype.slice.call(arguments,0)))}}var e=a;"undefined"!==typeof g?e=a[g]=[]:g="mixpanel";e.people=e.people||[];e.toString=function(b){var a="mixpanel";"mixpanel"!==g&&(a+="."+g);b||(a+=" (stub)");return a};e.people.toString=function(){return e.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");for(h=0;h<l.length;h++)c(e,l[h]);var f="set set_once union unset remove delete".split(" ");e.get_group=function(){function a(c){b[c]=function(){call2_args=arguments;call2=[c].concat(Array.prototype.slice.call(call2_args,0));e.push([d,call2])}}for(var b={},d=["get_group"].concat(Array.prototype.slice.call(arguments,0)),c=0;c<f.length;c++)a(f[c]);return b};a._i.push([b,d,g])};a.__SV=1.2;b=c.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===c.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";d=c.getElementsByTagName("script")[0];d.parentNode.insertBefore(b,d)}})(document,window.mixpanel||[]);
     }
     this.mixpanel.init(this.apiKey, this.options);
+    this.logger = options.logger;
   }
 
   alias(userId: string, previousId: string | undefined) {
@@ -42,14 +45,23 @@ export class MixpanelPlugin extends Plugin {
     }
 
     if (properties) {
-      this.mixpanel.people.set(properties);
+      const id = +new Date();
+      this.logger!.debug(`${this.id}: identify(request) ${id}: ${userId}, ${JSON.stringify(properties)}`);
+      this.mixpanel.people.set(properties, (payload: unknown) => {
+        this.logger!.debug(`${this.id}: identify(response) ${id}: done: ${JSON.stringify(payload)}`);
+      });
     }
   }
 
   track(userId: string | undefined, event: Event) {
+    const id = +new Date();
+    this.logger!.debug(`${this.id}: track(request) ${id}: ${userId}, ${JSON.stringify(event)}`);
     this.mixpanel.track(
       event.name,
       { ...event.properties },
+      (payload: unknown) => {
+        this.logger!.debug(`${this.id}: track(response) ${id}: done: ${JSON.stringify(payload)}`);
+      },
     );
   }
 
