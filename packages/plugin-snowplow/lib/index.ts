309e0e461731b6ce34e109a6b5aea7b2dbfe775a
Justin Fiedler <justin@iterative.ly>
2021-01-19 06:31:46 +0000

    feat: only configure plugins if instance is not provided ITLY-1510

--- packages/plugin-snowplow/lib/index.ts
+++ packages/plugin-snowplow/lib/index.ts
@@ -31,8 +31,8 @@ export class SnowplowPlugin extends Plugin {
       // @ts-ignore
       // eslint-disable-next-line
       ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)};p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","//d1fc8wv8zag5ca.cloudfront.net/2.10.2/sp.js","snowplow"));
+      this.snowplow('newTracker', 'itly', this.options.url, this.options.config);
     }
-    this.snowplow('newTracker', 'itly', this.options.url, this.options.config);
   }
 
   identify(userId: string | undefined, properties?: Properties) {
