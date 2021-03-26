df6959c4af8b37136b9f5714aa7f16075d74bc8d
Joe Reeve <joe@iterative.ly>
2021-03-26 17:13:56 +0000

    test: add tests for Google Analytics GTAG plugin

--- packages/plugin-google-analytics/lib/__tests__/smoke.test.ts
+++ packages/plugin-google-analytics/lib/__tests__/smoke.test.ts
@@ -1,10 +1,12 @@
 import { ItlyBrowser as Itly } from '@itly/sdk';
+import _GoogleAnalyticsPlugin from '../..';
 
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require, import/no-unresolved, import/extensions */
 import { requireForTestEnv } from '../../../../__tests__/util';
 
-const GoogleAnalyticsPlugin = requireForTestEnv(__dirname);
+type IGoogleAnalyticsPlugin = typeof _GoogleAnalyticsPlugin;
+const GoogleAnalyticsPlugin: IGoogleAnalyticsPlugin = requireForTestEnv(__dirname);
 
 let itly: Itly;
 
@@ -14,12 +16,8 @@ beforeEach(() => {
   itly = new Itly();
 });
 
-afterEach(() => {
-  itly = undefined;
-});
-
 test('should not crash on load', () => {
-  const googleAnalyticsPlugin = new GoogleAnalyticsPlugin({});
+  const googleAnalyticsPlugin = new GoogleAnalyticsPlugin({ measurementId: 'G-CC5SQPL3J6' });
 
   expect(() => {
     itly.load({
@@ -27,4 +25,53 @@ test('should not crash on load', () => {
       plugins: [googleAnalyticsPlugin],
     });
   }).not.toThrow();
+});
+
+test('loads global gtag', async () => {
+  const gaFn = jest.fn();
+  // eslint-disable-next-line no-restricted-globals
+  (self as any).gtag = gaFn;
+  const googleAnalyticsPlugin = new GoogleAnalyticsPlugin({ measurementId: 'G-CC5SQPL3J6' });
+
+  itly.load({
+    environment: 'production',
+    plugins: [googleAnalyticsPlugin],
+  });
+  itly.identify({ userId: 'user-1234' });
+  itly.track({ name: 'Test', properties: { number: 1, string: 'Hello' } });
+
+  expect(gaFn).toHaveBeenCalledTimes(2);
+});
+
+test('uses explicitly passed gtag', async () => {
+  const gtag = jest.fn();
+  const googleAnalyticsPlugin = new GoogleAnalyticsPlugin({ measurementId: 'G-CC5SQPL3J6', gtag });
+
+  itly.load({
+    environment: 'production',
+    plugins: [googleAnalyticsPlugin],
+  });
+  itly.identify({ userId: 'user-1234' });
+  itly.track({ name: 'Test', properties: { number: 1, string: 'Hello' } });
+
+  expect(gtag).toHaveBeenCalledTimes(2);
+});
+
+test('creates a global gtag', async () => {
+  const googleAnalyticsPlugin = new GoogleAnalyticsPlugin({ measurementId: 'G-CC5SQPL3J6' });
+  // eslint-disable-next-line no-underscore-dangle
+  expect((googleAnalyticsPlugin as any)._gtag).toBeDefined();
+  // eslint-disable-next-line no-underscore-dangle
+  (googleAnalyticsPlugin as any)._gtag = jest.fn(googleAnalyticsPlugin.gtag);
+  // eslint-disable-next-line no-restricted-globals
+  expect(self.gtag).toBeDefined();
+
+  itly.load({
+    environment: 'production',
+    plugins: [googleAnalyticsPlugin],
+  });
+  itly.identify({ userId: 'user-1234' });
+  itly.track({ name: 'Test', properties: { number: 1, string: 'Hello' } });
+
+  expect(googleAnalyticsPlugin.gtag).toHaveBeenCalledTimes(2);
 });
