e0c965505b277e30296f8965ea7860ac4454b222
Andrey Sokolov <falconandy@yandex.ru>
2021-05-13 20:01:23 +0400

    fix: added unit tests to test callbacks for AmplitudePlugin (browser)

--- packages/plugin-amplitude/lib/__tests__/unit.test.ts
+++ packages/plugin-amplitude/lib/__tests__/unit.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-unresolved, import/extensions, import/no-dynamic-require */
 import { Loggers, PluginLoadOptions } from '@itly/sdk';
-import AmplitudePlugin from '../index';
+import AmplitudePlugin, { AmplitudeCallback } from '../index';
 
 const apiKey = 'API-KEY';
 const pluginLoadOptions: PluginLoadOptions = { environment: 'production', logger: Loggers.None };
@@ -80,6 +80,8 @@ describe('load', () => {
 });
 
 describe('identify', () => {
+  const properties = { a: 123, b: 'abc' };
+
   test('should call internal setUserId() if userId is defined', () => {
     const plugin = new AmplitudePlugin(apiKey);
     plugin.load(pluginLoadOptions);
@@ -108,23 +110,65 @@ describe('identify', () => {
     };
     amplitudeObject.Identify = () => amplitudeIdentify;
 
-    plugin.identify(undefined, { a: 123, b: 'abc' });
+    plugin.identify(undefined, properties);
     expect(amplitudeObject.getInstance().setUserId).toHaveBeenCalledTimes(0);
     expect(amplitudeObject.getInstance().identify).toHaveBeenCalledTimes(1);
     expect(amplitudeIdentify.set).toHaveBeenCalledTimes(2);
     expect(amplitudeIdentify.set.mock.calls[0].slice(0, 2)).toEqual(['a', 123]);
     expect(amplitudeIdentify.set.mock.calls[1].slice(0, 2)).toEqual(['b', 'abc']);
   });
+
+  test('should call callback', (done) => {
+    const plugin = new AmplitudePlugin(apiKey);
+    plugin.load(pluginLoadOptions);
+
+    const amplitudeIdentify = {
+      set: jest.fn(),
+    };
+    amplitudeObject.Identify = () => amplitudeIdentify;
+    amplitudeObject.getInstance().identify = (identifyObject: any, callback: AmplitudeCallback) => {
+      callback(211, 'identify response', 'identify details');
+    };
+
+    plugin.identify(undefined, properties, {
+      callback: (statusCode: number, responseBody: string, details: unknown) => {
+        expect(statusCode).toBe(211);
+        expect(responseBody).toBe('identify response');
+        expect(details).toBe('identify details');
+        done();
+      },
+    });
+  });
 });
 
 describe('track', () => {
+  const event = { name: 'event-A', properties: { a: 'abc', b: 123 } };
+
   test('should call internal logEvent() with event name and properties', () => {
     const plugin = new AmplitudePlugin(apiKey);
     plugin.load(pluginLoadOptions);
 
-    plugin.track(undefined, { name: 'event-A', properties: { a: 'abc', b: 123 } });
+    plugin.track('user-1', event);
     expect(amplitudeObject.getInstance().logEvent).toHaveBeenCalledTimes(1);
     expect(amplitudeObject.getInstance().logEvent.mock.calls[0].slice(0, 2)).toEqual(['event-A', { a: 'abc', b: 123 }]);
+  });
+
+  test('should call callback', (done) => {
+    const plugin = new AmplitudePlugin(apiKey);
+    plugin.load(pluginLoadOptions);
+
+    amplitudeObject.getInstance().logEvent = (name: any, properties: any, callback: AmplitudeCallback) => {
+      callback(322, 'track response', 'track details');
+    };
+
+    plugin.track('user-1', event, {
+      callback: (statusCode: number, responseBody: string, details: unknown) => {
+        expect(statusCode).toBe(322);
+        expect(responseBody).toBe('track response');
+        expect(details).toBe('track details');
+        done();
+      },
+    });
   });
 });
 
